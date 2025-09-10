730a245b1f8fb3bc135681058e4cd660a253631d
xnovedov <xnovedov@gmail.com>
2025-09-11 04:50:17 +0500

    Update rayfield_menu.lua

--- rayfield_menu.lua
+++ rayfield_menu.lua
@@ -1,31 +1,50 @@
-local Rayfield = loadstring(game:HttpGet("https://raw.githubusercontent.com/ArowixExploits/RayfieldUILibrary/main/source"))()
+debugX = true
+
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
-    Name = "Debug Menu",
-    LoadingTitle = "Loading Debug Menu...",
-    LoadingSubtitle = "Please wait",
-    ConfigurationSaving = { Enabled = true, FolderName = nil, FileName = "DebugConfig" }
+   Name = "Rayfield Example Window",
+   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
+   LoadingTitle = "Rayfield Interface Suite",
+   LoadingSubtitle = "by Sirius",
+   Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
+
+   DisableRayfieldPrompts = false,
+   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
+
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = nil, -- Create a custom folder for your hub/game
+      FileName = "Big Hub"
+   },
+
+   Discord = {
+      Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
+      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
+      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+   },
+
+   KeySystem = false, -- Set this to true to use our key system
+   KeySettings = {
+      Title = "Untitled",
+      Subtitle = "Key System",
+      Note = "No method of obtaining the key is provided", -- Use this to tell the user how to get a key
+      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+   }
 })
 
-local ESPTab = Window:CreateTab("ESP")
-local ESPSection = ESPTab:CreateSection("Visuals")
+local Tab = Window:CreateTab("Tab Example", 4483362458) -- Title, Image
 
-ESPSection:CreateToggle({Name="ESP Box", Flag="ESPBox", CurrentValue=true})
-ESPSection:CreateToggle({Name="ESP Health", Flag="ESPHealth", CurrentValue=true})
-ESPSection:CreateToggle({Name="ESP Distance", Flag="ESPDistance", CurrentValue=true})
-ESPSection:CreateToggle({Name="ESP Skeleton", Flag="ESPSkeleton", CurrentValue=true})
+local Section = Tab:CreateSection("Section Example")
 
-Window:CreateKeybind({
-    Name = "Toggle Menu",
-    CurrentKey = Enum.KeyCode.RightShift,
-    HoldToInteract = false,
-    Flag = "ToggleMenu",
-    Callback = function()
-        local MainFrame = Window.MainFrame
-        if MainFrame then
-            MainFrame.Visible = not MainFrame.Visible
-        end
-    end
+local Button = Tab:CreateButton({
+   Name = "Button Example",
+   Callback = function()
+   -- The function that takes place when the button is pressed
+   end,
 })
 
-return Window
+Rayfield:LoadConfiguration()
