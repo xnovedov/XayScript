792f15f4eb24a1840da484ca66e8ef716613ead7
xnovedov <xnovedov@gmail.com>
2025-09-12 00:41:07 +0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
-local OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/xnovedov/XayScript/refs/heads/main/source.lu"))()
+local OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/xnovedov/XayScript/refs/heads/main/source.lua"))()
 
 local ESP_ENABLED = false
 local SHOW_BOX = false
