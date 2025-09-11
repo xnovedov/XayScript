d0abe8e06461a981faced897a359ddd8a35c90dd
xnovedov <xnovedov@gmail.com>
2025-09-11 12:49:32 +0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,2 +1,3 @@
 loadstring(game:HttpGet("https://github.com/xnovedov/XayScript/blob/main/esp.lua"))()
 loadstring(game:HttpGet("https://github.com/xnovedov/XayScript/blob/main/rayfield_menu.lua"))()
+loadstring(game:HttpGet("https://github.com/xnovedov/XayScript/blob/main/source.lua"))()
